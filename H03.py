b44d535e681b5844aa4b366bdc9bb348c1ef024d
Yuna <yuna@hkhk.edu.ee>
2024-10-16 11:28:12 +0300

    Update H03.py

--- H03.py
+++ H03.py
@@ -2,6 +2,20 @@
 #Ülesanded 03
 import turtle
 
+#Ülesane 03.5: unistuste auto
+mark, mudel = "bmw", "118"
+auto = mark+" "+mudel
+aasta = 1997
+hind = 301.60
+
+print("Minu unistuste auto on",auto,"aastast",aasta,"mille hind on",hind,"eurot.")
+
+
+
+
+
+
+""
 #Ülesanded 03.6: Python turtle kolmnurk
 kylje_pikkus = 200
 nurk = 120
