10367b3a03bae89999fdb9a954e72e90439c648d
Yuna <yuna@hkhk.edu.ee>
2024-10-16 11:46:46 +0300

    Create H04.py

---
+++ H04.py
@@ -0,0 +1,9 @@
+#16.10.2024 Yuna Antonenko
+#Ülesanded 4
+
+
+#1. Aia pikkus
+a = int(input("Sisesta külg 1: "))
+b = int(input("Sisesta külg 2: "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus om {p} meetrit.")
