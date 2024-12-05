083fdfa815bab7b9f2ce6df6cea5bb18c02f2c61
Yuna <yuna@hkhk.edu.ee>
2024-12-05 10:21:34 +0200

    update

---
+++ KT(iseseisev töö).py
@@ -0,0 +1,32 @@
+#05.12.2024 Yuna Antonenko
+#KT
+
+'''
+#1
+print("Tere, maailm!")
+
+#2
+aasta = 2020
+liblikas = "teelehe-mosaiikliblikas"
+lause_keskosa = "aasta liblikas on: "
+
+def lause():
+    print(aasta,lause_keskosa,liblikas)
+lause()
+'''
+#3
+
+kõrgus = int(input("Pilve kõrgus: "))
+
+if kõrgus >= 6:
+    print("Need on ülemised pilved")
+else:
+    print("Need ei ole ülemised pilved")
+    
+
+
+
+
+
+
+
