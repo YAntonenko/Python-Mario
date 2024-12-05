a152184e9fd44fa102524a6c20ccdc7ad978b966
Yuna <yuna@hkhk.edu.ee>
2024-12-05 10:39:15 +0200

    Update KT(iseseisev töö).py

--- KT(iseseisev töö).py
+++ KT(iseseisev töö).py
@@ -1,11 +1,13 @@
 #05.12.2024 Yuna Antonenko
 #KT
 
+
+#Iseseisev töö 1
 '''
-#1
+#
 print("Tere, maailm!")
 
-#2
+#
 aasta = 2020
 liblikas = "teelehe-mosaiikliblikas"
 lause_keskosa = "aasta liblikas on: "
@@ -13,9 +15,8 @@ lause_keskosa = "aasta liblikas on: "
 def lause():
     print(aasta,lause_keskosa,liblikas)
 lause()
-'''
-#3
 
+#
 kõrgus = int(input("Pilve kõrgus: "))
 
 if kõrgus >= 6:
@@ -23,6 +24,40 @@ if kõrgus >= 6:
 else:
     print("Need ei ole ülemised pilved")
     
+
+#
+inimesed = 60
+inimekohad = 40
+
+bussid = inimesed//inimekohad
+vimases_bussis = inimesed&inimekohad
+if vimases_bussis==0:
+    print(f"Selle arvu inimeste jaoks on vaja {bussid} buss, vimases bussion {inimekohad}")
+else:
+    print(f"Selle arvu inimeste jaoks on vaja {bussid+1} buss, vimases bussion {vimases_bussis}")
+
+'''
+
+
+#Iseseisev töö 2
+'''
+#
+kordade_arv = int(input("Sisseta mitu korda äratada: "))
+for i in range(kordade_arv):
+    print("Tõuse ja säru!")
+'''
+#
+ringide_arv = int(input("Sisseta ringide arv: "))
+koguarv = 0
+ring = 1
+while ring <= ringide_arv:
+    if ring % 2 == 0:
+        koguarv +- ring 
+    ring +- 1
+print(f"Porgandite koguarv on {koguarv}.")
+
+
+
 
 
 
