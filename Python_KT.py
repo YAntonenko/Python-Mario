365ff13b7575f700c432ee89a063409a5795735f
Yuna <yuna@hkhk.edu.ee>
2024-12-18 15:39:22 +0200

    Update Python_KT.py

--- Python_KT.py
+++ Python_KT.py
@@ -8,7 +8,7 @@
 #   пользователь вводит обычное слово, которое заменяется на секретный язык - 1p
 #   пользователь вводит слово на секретном языке, которое преобразуется обратно в обычный - 1р
 #   код с комментариями - 1р
-
+'''
 def create_cipher(text):
     return ''.join(chr(ord(char) + 1) for char in text)
 
@@ -16,14 +16,14 @@ def decode_cipher(ciphered_text):
     return ''.join(chr(ord(char) - 1) for char in ciphered_text)
 
 def main():
-    choice = input("Do you want to create a cipher? (yes or no):  ")
+    choice = input("Do you want to create a cipher (1) or  take the normal word back? (2):  ")
     
-    if choice == 'yes':
+    if choice == '1':
         normal_word = input("Enter a normal word to convert to cipher: ")
         ciphered_word = create_cipher(normal_word)
         print(f"Ciphered word: {ciphered_word}")
         
-    elif choice == 'no':
+    elif choice == '2':
         ciphered_word = input("Enter a ciphered word to convert back to normal: ")
         normal_word = decode_cipher(ciphered_word)
         print(f"Normal word: {normal_word}")
@@ -33,7 +33,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+'''
 
 #12 Eurokalkulaator
 
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 #   предыдущая проверка, не добавил ли пользователь число или не установил его равным нулю — 2p
 #   код, сообщающий о четном или нечетном числе - 2p
 #   отображается название программы - 1п
-'''
+
 def check_even_odd():
     print("Hey!")
     number = int(input("Please enter a number:  "))
@@ -87,7 +87,7 @@ def check_even_odd():
         print(f"The number {number} is odd.")
 
 check_even_odd()
-'''
+
 
 #14
 
