44323807b8ea41787d275d47d8ff0834dfe54d21
Yuna <yuna@hkhk.edu.ee>
2024-12-19 19:08:06 +0200

    update

--- Python_KT.py
+++ Python_KT.py
@@ -22,15 +22,12 @@ def main():
         normal_word = input("Enter a normal word to convert to cipher: ")
         ciphered_word = create_cipher(normal_word)
         print(f"Ciphered word: {ciphered_word}")
-        
     elif choice == '2':
         ciphered_word = input("Enter a ciphered word to convert back to normal: ")
         normal_word = decode_cipher(ciphered_word)
         print(f"Normal word: {normal_word}")
-        
     else:
         print("Invalid choice. Please select 1 or 2.")
-
 if __name__ == "__main__":
     main()
 '''
@@ -95,75 +92,86 @@ check_even_odd()
 #  (10 сотрудников).
 # Создайте программу, которая анализирует, есть ли в компании дискриминация по половому признаку. 
 # Для этого сравните самые высокие зарплаты n(naised) и m(mehed). Программа должна принять решение.
+'''
+with open("palk.txt", "r") as file:
+    for line in file:
+        file.write(f"{line[0]} {line[1]} {line[2]}\n")
 
-with open('palk.txt', 'w') as file:
-    file.write("Hubert Hunt m 2340\n")
-    file.write("Siim Siil m 2570\n")
-    file.write("Märt Mäger m 1960\n")
-    file.write("Vilma Vihmauss n 2060\n")
-    file.write("Merike Metskits n 2250\n")
-    file.write("Kati Karu n 2370\n")
-    file.write("Elmar Elevant m 2900\n")
-    file.write("Timoteus Tigu m 2850\n")
-    file.write("Reet Rebane n 2340\n")
-    file.write("Kalev Kaamel m 2570\n")
-    file.write("Karmen Kass n 2120\n")
-    file.write("Kornelius Koer m 2250\n")
-
-
-def analyze_discrimination(file_path):
-    m_palk = []
-    n_palk = []
+def analyze_pay(filename):
+    with open(filename, 'r') as file:
+        data = file.readlines()
+    mehed_palk = []
+    naised_palk = []
+    for line in data:
+        andmed = line.strip().split(', ')
+        nimi = float(andmed[1,2])
+        sugu = float(andmed[3])
+        palk = float(andmed[4])
+        if sugu == 'm':
+            mehed_palk.append(palk)
+        else:
+            naised_palk.append(palk)
+    mehed_avg = sum(mehed_palk) / len(mehed_palk) if mehed_palk else 0
+    naised_avg = sum(naised_palk) / len(naised_palk) if naised_palk else 0
     
-    with open(file_path, 'r') as file:
-        for line in file:
-            andmed = line.strip()
+    print(f"Meeste keskmine palk: {mehed_avg} €")
+    print(f"Naiste keskmine palk: {naised_avg} €")
     
-            nimi = float(andmed[1])
-            sugu = float(andmed[3])
-            palk = float(andmed[4])
-            
-            if sugu == 'm':
-                m_palk.append(palk)
-            else:
-                n_palk.append(palk)
-        print([nimi], [sugu], [palk])
-
-    avg_m_palk = sum(m_palk) / len(m_palk) if m_palk else 0
-    avg_n_palk = sum(n_palk) / len(n_palk) if n_palk else 0
-
-    if avg_m_palk > avg_n_palk:
-        print("Discrimination detected: Mehed earn more on average.")
-    elif avg_n_palk > avg_m_palk:
-        print("Discrimination detected: Naised earn more on average.")
+    if mehed_avg > naised_avg:
+        print("Mehed teenivad keskmiselt rohkem.")
+    elif naised_avg > mehed_avg:
+        print("Naised teenivad keskmiselt rohkem.")
     else:
-        print("No discrimination detected: Palkad are equal on average.")
+        print("Keskmised palgad on võrdsed.")
+file.close()
+'''
+#16 Täringud
+import random
 
-analyze_discrimination('palk.txt')
+class DiceGame:
+    def __init__(self):
+        self.user_name = input("Enter your name: ")
+        self.user_money = 500
+        self.computer_money = 500
+    
+    def roll_dice(self):
+        return random.randint(1, 6) + random.randint(1, 6) 
+    
+    def play_round(self):
+        print(f"\n{self.user_name}'s money: €{self.user_money} | Computer's money: €{self.computer_money}")
+        bet = int(input(f"{self.user_name}, place your bet (you have €{self.user_money}): "))
+        
+        if bet > self.user_money:
+            print("You can´t bet more than you have!")
+            return
+        user_roll = self.roll_dice()
+        computer_roll = self.roll_dice()
+        print(f"{self.user_name} roll: {user_roll}")
+        print(f"Computer roll: {computer_roll}")
 
+        if user_roll > computer_roll:
+            self.user_money += bet
+            self.computer_money -= bet
+            print(f"{self.user_name} win!")
+        elif user_roll < computer_roll:
+            self.user_money -= bet
+            self.computer_money += bet
+            print("Computer win!")
+        else:
+            print("It's tie!")
+    
+    def play_game(self):
+        while self.user_money > 0 and self.computer_money > 0:
+            self.play_round()
+        
+        if self.user_money <= 0:
+            print("You have run out of money! Game over.")
+        else:
+            print("Computer has run out of money! You win!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-sugu = m
-
-
-#16
-
-
-
-
-
+if __name__ == "__main__":
+    game = DiceGame()
+    game.play_game()
 
 
 
