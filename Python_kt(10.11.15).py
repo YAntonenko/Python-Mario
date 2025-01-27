63f65806e6969a62ea85ff16c5d80ab5eb4664bc
Yuna <yuna@hkhk.edu.ee>
2025-01-27 11:38:55 +0200

    update

---
+++ Python_kt(10.11.15).py
@@ -0,0 +1,35 @@
+#18.12.2024 Yuna Antonenko
+#KT
+#10 11 15
+
+
+#10. Kaugushüpe
+"""
+tulemused = []
+
+for i in range(3):
+    tulemus = float(input(f"Sisestage kaugushüppe tulemus {i + 1}: "))
+    tulemused.append(tulemus)
+
+parim_tulemus = max(tulemused)
+keskmine_tulemus = sum(tulemused) / len(tulemused)
+
+print(f'Parim kaugushüppe tulemus: {parim_tulemus} m')
+print(f'Keskmine kaugushüppe tulemus: {keskmine_tulemus} m')
+"""
+
+#11. Salakeel
+
+
+
+#15. Temperatuurid
+with open('temperatuurid.txt', 'r') as fail:
+        kuupaev = rida.strip().split()
+        kuu = kuupaev[:12]
+        temp = float(temp)
+
+
+print(f"{kuu}: {kuupaev}, {temp}")
+
+
+
