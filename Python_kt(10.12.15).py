d4588409476a69114edecb6c46817cf42e5de944
Yuna <yuna@hkhk.edu.ee>
2025-01-27 13:39:00 +0200

    Update Python_kt(10.12.15).py

--- Python_kt(10.12.15).py
+++ Python_kt(10.12.15).py
@@ -16,7 +16,7 @@ keskmine_tulemus = sum(tulemused) / len(tulemused)
 
 print(f'Parim kaugushüppe tulemus: {parim_tulemus} m')
 print(f'Keskmine kaugushüppe tulemus: {keskmine_tulemus} m')
-"""
+
 
 #12. Eurokalkulaator
 valik  = input("Vali suund (1: EUR -> EEK, 2: EEK -> EUR): ")
@@ -31,15 +31,14 @@ elif valik == "2":
     print(f"{eek} EEK on {eur} EUR")
 else:
     print("Vale valik")
-
 """
 #15. Temperatuurid
 for rida in open('temperatuurid.txt', 'r', encoding= 'UTF-8'):
     osad = rida.split()
-    kuu = osad[1]
-    temp = map(int, osad[2:])
+    kuu = osad[0]
+    temp = map(int, osad[1:])
     print(f"{kuu}: {max(temp)}")
-"""
+
 
 
 
